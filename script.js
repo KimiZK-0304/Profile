c534f826940e370b8610eb241dd02c4334810d21
KimiZK-0304 <lebinhgaming999lk@gmail.com>
2024-04-22 08:38:00 +0700

    profile

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const bgCard =
 const avtCard =
   "https://i.pinimg.com/564x/2e/d7/30/2ed730d06ab204ac4442cac832ffa99b.jpg";
 const verified = "https://i.ibb.co/ckpym8q/snapedit-1713704525392.png";
+const picPost = "https://i0.wp.com/halcyonrealms.com/blogpics/japgifA.gif?resize=500%2C288&ssl=1"
+const title = "Chill with me!~"
+const des = "Sex"
 
 d.querySelector(".nickName").innerHTML = nickName;
 d.querySelector(".name").innerHTML = fullName;
@@ -21,5 +24,6 @@ d.querySelector(".verified img").src = verified;
 d.querySelector(".btn-follow").href = URL_FB;
 d.querySelector(".btn-chat").href = URL_CHAT;
 
-d.querySelector(".title").innerHTML = `Title: Hi`;
-d.querySelector(".des").innerHTML = `Description: OK`;
+d.querySelector(".title").innerHTML = `Tiêu đề: ${title}`;
+d.querySelector(".des").innerHTML = `Mô tả: ${des}`;
+d.querySelector('.picPost img').src = picPost
